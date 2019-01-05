14aa65c7202a0427b90e6a7c8e63402fcac20f6b
takuya.ishida <byhpw107@gmail.com>
2019-01-05 12:45:40 +0000

    follow,unfollow

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="row">
-        <div class="col-sm-6 col-sm-offset-3">
+        <div class="col-sm-6 offset-sm-3">
 
             {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
